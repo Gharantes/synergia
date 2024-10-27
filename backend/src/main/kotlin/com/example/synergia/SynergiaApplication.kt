059cc5f72f431d2ff3405783d9558c06dc7d336c
Gharantes <work.gharantes@gmail.com>
2024-10-27 20:48:01 -0300

    Open api configuration

--- backend/src/main/kotlin/com/example/synergia/SynergiaApplication.kt
+++ backend/src/main/kotlin/com/example/synergia/SynergiaApplication.kt
@@ -1,20 +1,11 @@
 package com.example.synergia
 
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SynergiaApplication
-	: ApplicationRunner
-{
-	override fun run (args: ApplicationArguments) {
-		println("Teste")
-	}
-}
+class FuncionalFretesExportacaoApplication
 
 fun main(args: Array<String>) {
-	runApplication<SynergiaApplication>(*args)
+	runApplication<FuncionalFretesExportacaoApplication>(*args)
 }
